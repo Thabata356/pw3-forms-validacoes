153f25bd01c0aa2fcd356526c5d6f68187869887
Thabata356 <thabatav7@gmail.com>
2025-05-20 11:34:02 -0300

    :sparkles: Feat: Criar o projeto pw3-form-validacoes e fazer a validação dos campos.

---
+++ pw3-form-validacoes/src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-cadastro',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './cadastro.component.html',
+  styleUrl: './cadastro.component.css'
+})
+export class CadastroComponent {
+
+  formulario: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.formulario = this.fb.group({
+      nome: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['',[Validators.email, Validators.required]],
+      idade: [null,[Validators.min(18), Validators.required]],
+    });
+  }
+
+  onSubmit() {
+      alert('Formulário enviado com sucesso!');
+  }
+
+}
